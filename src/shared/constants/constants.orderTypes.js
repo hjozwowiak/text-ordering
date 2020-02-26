9db99f347eee4a1c28d007ece541356454c17656
Hubert Józwowiak <33119380+hjozwowiak@users.noreply.github.com>
2020-02-26 09:37:12 +0100

    Update constants.orderTypes.js

--- src/shared/constants/constants.orderTypes.js
+++ src/shared/constants/constants.orderTypes.js
@@ -72,7 +72,6 @@ export const orderTypes = {
         name: "przeredagowanie i rozszerzenie obecnej treści",
         fields: [
             "url",
-            "charactersToExtendTo",
             "charactersToWrite",
             "h1",
             "hx",
