b2d6bfaa3cfd60dd965db15475a36ef67e4bf40c
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2020-03-31 02:17:25 +0200

    Changed formatting of AlertModal component

--- src/components/AlertModal/AlertModal.js
+++ src/components/AlertModal/AlertModal.js
@@ -1,61 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
 } from "@material-ui/core";
 
 const AlertModal = ({
-    open,
-    messageHead,
-    messageBody,
-    handleClose,
-    customAction
+  open,
+  messageHead,
+  messageBody,
+  handleClose,
+  customAction
 }) => {
-    const handleConfirmClick = (handleClose, customAction) => {
-        if (customAction) customAction();
-        handleClose();
-    };
+  const handleConfirmClick = (handleClose, customAction) => {
+    if (customAction) customAction();
+    handleClose();
+  };
 
-    return (
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{messageHead}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {messageBody}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Anuluj</Button>
+        <Button
+          onClick={() => {
+            handleConfirmClick(handleClose, customAction);
+          }}
+          color="primary"
+          autoFocus
         >
-            <DialogTitle id="alert-dialog-title">{messageHead}</DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    {messageBody}
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Anuluj</Button>
-                <Button
-                    onClick={() => {
-                        handleConfirmClick(handleClose, customAction);
-                    }}
-                    color="primary"
-                    autoFocus
-                >
-                    Potwierdzam
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+          Potwierdzam
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 AlertModal.propTypes = {
-    open: PropTypes.bool,
-    messageHead: PropTypes.string,
-    messageBody: PropTypes.string,
-    handleClose: PropTypes.func,
-    customAction: PropTypes.func
+  open: PropTypes.bool,
+  messageHead: PropTypes.string,
+  messageBody: PropTypes.string,
+  handleClose: PropTypes.func,
+  customAction: PropTypes.func
 };
 
 export default AlertModal;
