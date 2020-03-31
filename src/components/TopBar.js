d30714aa0e7c9ca736ecdd0269bb11cbc8af845b
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2020-03-31 02:42:10 +0200

    Refactored DescriptionLengthSlider component

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,118 +1,120 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "../style/TopBar.scss";
-import MetaDescLengthModifier from "./topBarSettings/MetaDescLengthModifier";
 import ThemeCustomizer from "./topBarSettings/ThemeCustomizer";
 import { Button, ButtonGroup } from "@material-ui/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCog,
-    faBroom,
-    faMinusSquare,
-    faPlusSquare,
-    faCopy
+  faCog,
+  faBroom,
+  faMinusSquare,
+  faPlusSquare,
+  faCopy
 } from "@fortawesome/free-solid-svg-icons";
 
+import DescriptionLengthSlider from "./DescriptionLengthSlider/DescriptionLengthSlider";
+
+import "../style/TopBar.scss";
+
 class TopBar extends Component {
-    state = {
-        settingsHidden: true,
-        topBarImgsNum: 10,
-        topBarImgName: "loading.gif"
-    };
+  state = {
+    settingsHidden: true,
+    topBarImgsNum: 10,
+    topBarImgName: "loading.gif"
+  };
 
-    toggleHidden = () => {
-        this.setState({
-            settingsHidden: !this.state.settingsHidden
-        });
-    };
+  toggleHidden = () => {
+    this.setState({
+      settingsHidden: !this.state.settingsHidden
+    });
+  };
 
-    handleChange = (event, newValue) => {
-        this.setState({ metaDescLength: newValue });
-    };
+  handleChange = (event, newValue) => {
+    this.setState({ metaDescLength: newValue });
+  };
 
-    render() {
-        const { settingsHidden } = this.state;
-        const {
-            handleClearButtonClick,
-            settings,
-            handleChangeThemeTypeSwitch,
-            updateMetaDescLength,
-            topBarImgName,
-            handleToggleFoldAllButtonClick
-        } = this.props;
+  render() {
+    const { settingsHidden } = this.state;
+    const {
+      handleClearButtonClick,
+      settings,
+      handleChangeThemeTypeSwitch,
+      updateMetaDescLength,
+      topBarImgName,
+      handleToggleFoldAllButtonClick
+    } = this.props;
 
-        let visibilityClass = "TopBar--settings-hidden";
-        if (settingsHidden === false) {
-            visibilityClass = "";
-        }
-
-        return (
-            <div className="TopBar row">
-                <div className="TopBar--options ">
-                    <div className="TopBar--imgContainer">
-                        <img
-                            className="imgContainer--img"
-                            alt="gfy"
-                            src={"/img/" + topBarImgName}
-                        />
-                    </div>
-                    <h1 className="TopBar--header">Template generator</h1>
-                    <ButtonGroup
-                        className="TopBar--optionsButtons"
-                        size="small"
-                        aria-label="small outlined button group"
-                        color="secondary"
-                    >
-                        <Button
-                            title="Rozwiń wszystkie"
-                            onClick={() => {
-                                handleToggleFoldAllButtonClick("expand");
-                            }}
-                        >
-                            <FontAwesomeIcon icon={faPlusSquare} />
-                        </Button>
-                        <Button
-                            title="Zwiń wszystkie"
-                            onClick={() => {
-                                handleToggleFoldAllButtonClick("fold");
-                            }}
-                        >
-                            <FontAwesomeIcon icon={faMinusSquare} />
-                        </Button>
-                        <Button
-                            title="Wyczyść wszystkie dane zamówienia"
-                            onClick={handleClearButtonClick}
-                        >
-                            <FontAwesomeIcon icon={faBroom} />
-                        </Button>
-                        <Button title="Ustawienia" onClick={this.toggleHidden}>
-                            <FontAwesomeIcon icon={faCog} />
-                        </Button>
-                    </ButtonGroup>
-                </div>
-                <div className={`TopBar--settings ${visibilityClass}`}>
-                    <MetaDescLengthModifier
-                        value={settings.metaDescLength}
-                        onChange={(_, arr) => {
-                            updateMetaDescLength(arr);
-                        }}
-                    />
-                    <ThemeCustomizer
-                        colorTheme={settings.colorTheme}
-                        onChange={handleChangeThemeTypeSwitch}
-                    />
-                </div>
-            </div>
-        );
+    let visibilityClass = "TopBar--settings-hidden";
+    if (settingsHidden === false) {
+      visibilityClass = "";
     }
+
+    return (
+      <div className="TopBar row">
+        <div className="TopBar--options ">
+          <div className="TopBar--imgContainer">
+            <img
+              className="imgContainer--img"
+              alt="gfy"
+              src={"/img/" + topBarImgName}
+            />
+          </div>
+          <h1 className="TopBar--header">Template generator</h1>
+          <ButtonGroup
+            className="TopBar--optionsButtons"
+            size="small"
+            aria-label="small outlined button group"
+            color="secondary"
+          >
+            <Button
+              title="Rozwiń wszystkie"
+              onClick={() => {
+                handleToggleFoldAllButtonClick("expand");
+              }}
+            >
+              <FontAwesomeIcon icon={faPlusSquare} />
+            </Button>
+            <Button
+              title="Zwiń wszystkie"
+              onClick={() => {
+                handleToggleFoldAllButtonClick("fold");
+              }}
+            >
+              <FontAwesomeIcon icon={faMinusSquare} />
+            </Button>
+            <Button
+              title="Wyczyść wszystkie dane zamówienia"
+              onClick={handleClearButtonClick}
+            >
+              <FontAwesomeIcon icon={faBroom} />
+            </Button>
+            <Button title="Ustawienia" onClick={this.toggleHidden}>
+              <FontAwesomeIcon icon={faCog} />
+            </Button>
+          </ButtonGroup>
+        </div>
+        <div className={`TopBar--settings ${visibilityClass}`}>
+          <DescriptionLengthSlider
+            value={settings.metaDescLength}
+            onChange={(_, arr) => {
+              updateMetaDescLength(arr);
+            }}
+          />
+          <ThemeCustomizer
+            colorTheme={settings.colorTheme}
+            onChange={handleChangeThemeTypeSwitch}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 TopBar.propTypes = {
-    handleClearButtonClick: PropTypes.func,
-    settings: PropTypes.object,
-    updateMetaDescLength: PropTypes.func,
-    topBarImgName: PropTypes.string,
-    handleChangeThemeTypeSwitch: PropTypes.func
+  handleClearButtonClick: PropTypes.func,
+  settings: PropTypes.object,
+  updateMetaDescLength: PropTypes.func,
+  topBarImgName: PropTypes.string,
+  handleChangeThemeTypeSwitch: PropTypes.func
 };
 
 export default TopBar;
--- src/components/topBarSettings/ThemeCustomizer.js
+++ src/components/topBarSettings/ThemeCustomizer.js
@@ -1,30 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Paper, Typography, FormControlLabel, Switch } from "@material-ui/core";
+import { Typography, FormControlLabel, Switch } from "@material-ui/core";
+
+import Paper from "../Paper/Paper";
 
 const ThemeCustomizer = ({ colorTheme, onChange }) => {
-    return (
-        <Paper className="Paper">
-            <Typography gutterBottom>Ustawienia motywu:</Typography>
-            <div>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            color="primary"
-                            onChange={onChange}
-                            checked={colorTheme.darkMode}
-                        />
-                    }
-                    label="Dark mode"
-                />
-            </div>
-        </Paper>
-    );
+  return (
+    <Paper>
+      <Typography gutterBottom>Ustawienia motywu:</Typography>
+      <div>
+        <FormControlLabel
+          control={
+            <Switch
+              color="primary"
+              onChange={onChange}
+              checked={colorTheme.darkMode}
+            />
+          }
+          label="Dark mode"
+        />
+      </div>
+    </Paper>
+  );
 };
 
 ThemeCustomizer.propTypes = {
-    colorTheme: PropTypes.object,
-    onChange: PropTypes.func
+  colorTheme: PropTypes.object,
+  onChange: PropTypes.func
 };
 
 export default ThemeCustomizer;
